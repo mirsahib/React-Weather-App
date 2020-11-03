01813c3870473cca360a0370c5e2dee061901268
mirsahib <mirsahib24@gmail.com>
2020-11-03 20:50:02 +0600

    :construction: Working on Home state

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import Header from "./components/header";
 import SearchBar from "./components/SearchBar";
 import Home from "./components/Home";
 import Footer from "./components/footer";
+import Axios from "axios";
 
 function App() {
   const [cityName, setCityName] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
 
   //function to track change in the search bar
   const handleChange = (e) => {
@@ -14,8 +16,32 @@ function App() {
   //function to perfom api request
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(cityName);
     setCityName("");
+    getWeatherData();
+  };
+
+  const getWeatherData = async () => {
+    console.log("getWeather is called");
+    const api = {
+      base: "https://api.openweathermap.org/data/2.5/",
+      key: process.env.REACT_APP_API_KEY,
+    };
+    // const data = await Axios.get(
+    //   `${api.base}forecast/?q=${cityName}&units=metric&appid=${api.key}`
+    // );
+    const data = JSON.parse(localStorage.getItem("weather-data"));
+    let list = data.data.list;
+    let timestamp = [];
+    let newList = [];
+    list.forEach((e) => {
+      const time_txt = e.dt_txt.split(" ")[0];
+      if (!timestamp.includes(time_txt)) {
+        timestamp.push(time_txt);
+        newList.push(e);
+      }
+    });
+    data.data.list = newList;
+    setWeatherData(data);
   };
 
   return (
@@ -26,7 +52,7 @@ function App() {
         onChangeValue={handleChange}
         handleSubmit={handleSubmit}
       />
-      <Home />
+      {weatherData ? <Home weatherData={weatherData} /> : ""}
       <Footer />
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,86 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ForcastTable from "./ForcastTable";
 
 function Home(props) {
+  const [state, setState] = useState({
+    city: "",
+    weekday: "",
+    date: "",
+    month: "",
+    temp: "",
+    wind: "",
+    rain: "",
+    wind_direction: "",
+  });
+  const extractData = () => {
+    let city = props.weatherData.data.city.name;
+    let timeStamp = new Date(props.weatherData.data.list[0].dt_txt);
+    let date = timeStamp.getDate();
+    let weekday = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ][timeStamp.getDay()];
+    let month = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sept",
+      "Oct",
+      "Nov",
+      "Dec",
+    ][timeStamp.getMonth()];
+    let temp = props.weatherData.data.list[0].main.temp;
+    let wind = (
+      props.weatherData.data.list[0].wind.speed *
+      (3600 / 1000)
+    ).toFixed(2);
+    let rain =
+      props.weatherData.data.list[0].rain !== undefined
+        ? props.weatherData.data.list[0].rain["3h"] * 100
+        : "";
+    let compassSector = [
+      "N",
+      "NNE",
+      "NE",
+      "ENE",
+      "E",
+      "ESE",
+      "SE",
+      "SSE",
+      "S",
+      "SSW",
+      "SW",
+      "WSW",
+      "W",
+      "WNW",
+      "NW",
+      "NNW",
+      "N",
+    ];
+    let wind_direction =
+      compassSector[
+        (props.weatherData.data.list[0].wind.deg / 22.5).toFixed(0)
+      ];
+    console.log(
+      `city ${city},weekday ${weekday},date ${date},month ${month},rain ${rain},temp ${temp},wind ${wind},wind_dir ${wind_direction}`
+    );
+    setState({ city, weekday, date, month, rain, temp, wind, wind_direction });
+    console.log(state);
+  };
+  useEffect(() => {
+    extractData();
+  }, []);
+
   return (
     <div className="forecast-table">
       <div className="container">
